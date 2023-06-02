aeda3983a8fdedf01cedb22e09ee29dd64083ba5
nsrnkha <2006154@itg.ac.id>
2023-06-02 18:04:40 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:simple_notes/utils/app_routes.dart';
+import 'db/database_service.dart';
+import 'models/note.dart';
+
+void main() async {
+  await Hive.initFlutter();
+  Hive.registerAdapter(NoteAdapter());
+  await Hive.openBox(DatabaseService.boxName);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      title: 'Simple Note',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+        useMaterial3: true,
+      ),
+      routeInformationParser: AppRoutes.goRouter.routeInformationParser,
+      routeInformationProvider: AppRoutes.goRouter.routeInformationProvider,
+      routerDelegate: AppRoutes.goRouter.routerDelegate,
+    );
+  }
+}
